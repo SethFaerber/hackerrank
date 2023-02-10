17fe6194104ed97118c802d795901186ada78b6d
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-02-10 10:44:02 -0600

    adds 5 tests. times method is invalid

--- lib/factor_finder.rb
+++ lib/factor_finder.rb
@@ -2,10 +2,30 @@ class FactorFinder
 
   attr_accessor :factors_array
   attr_accessor :multiples_array
+  attr_accessor :candidates
 
   def initialize(factors_array, multiples_array)
     @factors_array = factors_array
     @multiples_array = multiples_array
+    @candidates = []
+
   end
 
+  def candidates_first_element
+    @factors_array.pop
+  end
+
+  def candidates_last_element
+    @multiples_array.shift
+  end
+
+  def number_of_times
+    candidates_last_element/candidates_first_element
+  end
+
+  def add_candidates
+    number_of_times.times do |n|
+      @candidates << (candidates_first_element * (n + 1))
+    end
+  end
 end
--- spec/factor_finder_spec.rb
+++ spec/factor_finder_spec.rb
@@ -1,13 +1,36 @@
 require 'factor_finder'
 
 RSpec.describe FactorFinder do
-  let(:factors) { FactorFinder.new([2, 4], [16, 32, 96]) }
+  let(:object) { FactorFinder.new([2, 4], [16, 32, 96]) }
 
   it "creates a 'factors' array from the first parameter" do
-    expect(factors.factors_array).to eq([2, 4])
+    expect(object.factors_array).to eq([2, 4])
   end
 
-  it "creates a 'multiples' array from the second parameter" do
-    expect(factors.multiples_array).to eq([16, 32, 96])
+  it "captures the last element of the factors array" do
+    expect(object.candidates_first_element).to eq(4)
   end
+
+  it "captures the first element of the multiples array" do
+    expect(object.candidates_last_element).to eq(16)
+  end
+
+  it "finds how many times the first element goes into the last element" do
+    expect(object.number_of_times).to eq(4)
+  end
+
+  it "fills the cadidates array with all multiples of the first element up to the last element" do
+    expect(object.add_candidates).to eq([4, 8, 12, 16])
+  end
+
+  # it "creates a 'multiples' array from the second parameter" do
+  #   object.create_candidates
+  #
+  #   expect(object.multiples_array).to eq([16, 32, 96])
+  # end
+
+
+  # it "creates a 'candidates' array of all the multiples of the last value of the factors array to the first value of the multiples array" do
+  #   expect(object.candidates).to eq([4, 8, 12, 16])
+  # end
 end
