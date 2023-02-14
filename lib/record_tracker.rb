0160715477b59f7bdaedbcca37e6721ad3e22b55
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-02-14 16:08:31 -0600

    some bug in testing

--- lib/record_tracker.rb
+++ lib/record_tracker.rb
@@ -1,39 +1,56 @@
 class RecordTracker
-  attr_reader :scores
-  attr_reader :high_score
-  attr_reader :low_score
-  attr_reader :high_score_tracker
+  attr_reader :game_scores
+  attr_reader :current_high_score
+  attr_reader :current_low_score
+  attr_reader :high_score_count
+  attr_reader :low_score_count
 
   def initialize(scores)
-    @scores = scores
-    @high_score = 0
-    @low_score = 0
-    @high_score_tracker = 0
+    @game_scores = scores
+    @current_high_score = 0
+    @current_low_score = 0
+    @high_score_count = 0
+    @low_score_count = 0
   end
 
   def call
-    set_scores
-    increment_high_score
+    set_initial_scores
+    check_for_high_scores
+    @current_high_score = @game_scores[0]
+    check_for_low_scores
   end
 
-  def set_scores
-    @high_score = @low_score = @scores[0]
+  def set_initial_scores
+    @current_high_score = @current_low_score = @game_scores[0]
   end
 
-  def increment_high_score
+  def check_for_high_scores
+    @game_scores.each { |game_score| count_high_score(game_score) if game_score > @current_high_score }
 
-    @scores.each { |score| count_high_score(score) if score > @high_score }
-
-    @high_score_tracker
+    @high_score_count
   end
 
-  def count_high_score(score)
-    @high_score_tracker += 1
-    set_current_high_score(score)
+  def count_high_score(game_score)
+    @high_score_count += 1
+    set_current_high_score(game_score)
   end
 
-  def set_current_high_score(score)
-    @high_score = score
+  def set_current_high_score(game_score)
+    @current_high_score = game_score
   end
 
+  def check_for_low_scores
+    @game_scores.each { |game_score| count_low_score(game_score) if game_score < @current_low_score }
+
+    @low_score_count
+  end
+
+  def count_low_score(game_score)
+    @low_score_count += 1
+    set_current_low_score(game_score)
+  end
+
+  def set_current_low_score(game_score)
+    @current_low_score = game_score
+  end
 end
