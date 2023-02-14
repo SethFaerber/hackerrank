cce4d98209095d8fe4549ce44fec60865b389338
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-02-14 13:30:15 -0600

    the perfect refactor

--- lib/factor_finder.rb
+++ lib/factor_finder.rb
@@ -10,30 +10,31 @@ class FactorFinder
     @answers = []
   end
 
-  def find_lcm
-    @factors_array.reduce(@factors_array.first, :lcm)
-  end
-
-  def find_gcd
-    @multiples_array.reduce(@multiples_array.last, :gcd)
-  end
-
-  def number_of_candidates
-    find_gcd/find_lcm
-  end
-
-  def create_candidates
-    number_of_candidates.times do |n|
-      @candidates << (n + 1) * find_lcm
-    end
-    @candidates
+  def count_answers
+    select_candidates.count
   end
 
   def select_candidates
     create_candidates.select { |candidate| @multiples_array.all? { |multiple| multiple%candidate == 0 } }
   end
 
-  def count_answers
-    select_candidates.count
+  def create_candidates
+    number_of_candidates.times do |n|
+      @candidates << (n + 1) * find_lcm # This method call in a loop means it will be repeatedly called. Instead of that, we memoize this method.
+    end
+    @candidates
+  end
+
+  def number_of_candidates
+    find_gcd/find_lcm
+  end
+
+  def find_gcd
+    @multiples_array.reduce(@multiples_array.last, :gcd)
+  end
+
+  # We memoize the find_lcm method so that it runs the first time it is called and stores the return, so the next time it is called, it does not run, it just returns its original return.
+  def find_lcm
+    @find_lcm ||= @factors_array.reduce(@factors_array.first, :lcm)
   end
 end
